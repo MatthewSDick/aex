29329fbb9f59187b98dfbc1c145adfcc5c078b4a
MatthewSDick <matthew.dick@outlook.com>
2020-10-09 20:51:29 -0400

    Initial Save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import aex from './aex.png'
+import idp from './idp.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Future home of Antietam Exchange Club</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={aex} alt="logo" />
+        <img src={idp} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
